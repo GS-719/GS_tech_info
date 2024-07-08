42499bc515953bdf9a4c8b730ef22f973f11ba7c
GS-719 <134775577+GS-719@users.noreply.github.com>
2024-07-08 16:30:07 +0530

    Update Footer.js

--- app/Components/Footer.js
+++ app/Components/Footer.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
 import "../styles/Footer.css"
 
 const Footer = () => {
     return (
         <>
-            <hr/>
+            <hr />
             <div className='Footer'>
                 <div className="logo">
                     <p className='GS_Logo'>GS_tech-info</p>
+                </div>
+                <div>
+                    <div>
+                        <ul className='ul_tag'>
+                            <li className='li_tag'>
+                                <Link className='link' href="/Privacy">Privacy Policy</Link>
+                            </li>
+                            <li className='li_tag'>
+                                <Link className='link' href="/Term_of_service">Terms of Services</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div>&copy; 2024</div>
             </div>
