eb7ec3c72126cdd8e3562d7bd9f63533e6111464
GS-719 <134775577+GS-719@users.noreply.github.com>
2024-07-19 17:22:37 +0530

    Create CodeBox.js

---
+++ app/styles/CodeBox.js
@@ -0,0 +1,9 @@
+.codeBox {
+    background-color: #f4f4f4;
+    padding: 15px;
+    border-radius: 5px;
+    border: 1px solid #ddd;
+    overflow-x: auto;
+    font-family: 'Courier New', Courier, monospace;
+  }
+  
