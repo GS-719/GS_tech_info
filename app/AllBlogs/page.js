f0050c877405fced5ceff726dad489cb0d2845ea
GS-719 <134775577+GS-719@users.noreply.github.com>
2024-07-24 17:00:58 +0530

    Update page.js

--- app/AllBlogs/page.js
+++ app/AllBlogs/page.js
@@ -23,7 +23,7 @@ const page = () => {
                             <h1 className='E_Title'>VS Code the Ultimate Code editor</h1>
                         </div>
                     </Link>
-                    <Link href="blogs/Datatypes_in_python_Part_1" >
+                    <Link href="blogs/Datatypes_in_python_part_1" >
                         <div className='Element'>
                             <Image src={Datatype_P1} alt="error" className='E_IMG' />
                             <h1 className='E_Title'>Datatypes in python Part 1</h1>
