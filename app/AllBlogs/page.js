adcba613260dae8575fe60cb55b9a3080f6473fd
GS-719 <134775577+GS-719@users.noreply.github.com>
2024-07-24 16:52:58 +0530

    Update page.js

--- app/AllBlogs/page.js
+++ app/AllBlogs/page.js
@@ -2,7 +2,8 @@ import React from 'react'
 import "../styles/AllBlogs.css"
 import Image from "next/image";
 import Windows_VS_Linux from "../../Img/Title/Windows_VS_Linux.png";
-import VS_code from "../../Img/Title/VS_Code.png";
+import VS_code from "../../Img/Title/VS_code.png";
+import Datatype_P1 from "../../Img/Title/Datatypes_in_python_Part_1.png"
 import Link from "next/link";
 
 const page = () => {
@@ -20,6 +21,12 @@ const page = () => {
                         <div className='Element'>
                             <Image src={VS_code} alt="error" className='E_IMG' />
                             <h1 className='E_Title'>VS Code the Ultimate Code editor</h1>
+                        </div>
+                    </Link>
+                    <Link href="blogs/Datatypes_in_python_Part_1" >
+                        <div className='Element'>
+                            <Image src={Datatype_P1} alt="error" className='E_IMG' />
+                            <h1 className='E_Title'>Datatypes in python Part 1</h1>
                         </div>
                     </Link>
                 </div>
