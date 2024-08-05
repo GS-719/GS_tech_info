ab749f682e83bb238c760be8cfa2a861ecb37b17
GS-719 <134775577+GS-719@users.noreply.github.com>
2024-08-05 16:06:22 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,14 +9,19 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "GS_tech-info",
-  description: "Powered by GS",
+  title: "GS_tech_info",
+  description: "The GS_tech-info provides you 100% true information about computers",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta property="og:title" content="GS Tech Info"/>
+        <meta property="og:description" content="The GS_tech-info serves you the 100% accurate information about computer technologies, and sky rocket your computer journey"/>
+        {/* <meta property="og:url" content="https://gs-tech-info.vercel.app/"/> */}
+        <meta property="og:type" content="website"/>
+        <meta name="description" content="The GS_tech-info provides you 100% true information about computers"/>
         <meta name="google-adsense-account" content="ca-pub-3294386833309868" />
         <meta name="google-site-verification" content="1fckTI0MM1h8fwBgsl084W1Ew-Y4wrbyTF_MOhGpmjQ" />
       </head>
