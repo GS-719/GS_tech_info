ac9b125a35705f9d112d1dcc9699fab1996660b9
GS-719 <134775577+GS-719@users.noreply.github.com>
2024-08-05 16:06:38 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,10 +3,40 @@ import "./styles/Home_page.css";
 import Image from "next/image";
 import Home_1 from "../public/Home_1.jpg";
 import Link from 'next/link';
+import Head from 'next/head';
+import { NextSeo } from 'next-seo';
 
 const Home = () => {
   return (
     <>
+      <Head>
+        <NextSeo
+          title="GS Tech Info"
+          description="The GS_tech-info provides you 100% true information about computers"
+          canonical="https://gs-tech-info.vercel.app/"
+          openGraph={{
+            url: 'https://gs-tech-info.vercel.app/',
+            title: 'GS Tech Info',
+            description: 'The GS_tech-info provides you 100% true information about computers',
+            images: [
+              {
+                url: 'https://gs-tech-info.vercel.app/LOGO.png',
+                width: 800,
+                height: 600,
+                alt: 'Logo',
+                type: 'image/jpeg/png',
+              },
+            ],
+            siteName: 'GS Tech Info',
+          }}
+          twitter={{
+            handle: '@handle',
+            site: '@site',
+            cardType: 'summary_large_image',
+          }}
+        />
+        <meta property="og:url" content="https://gs-tech-info.vercel.app/" />
+      </Head>
       <div className='Main'>
         <section className='Left_Section'>
           <h1 className='Main_title'> The GS_tech-info provides you 100% true information about computers</h1>
