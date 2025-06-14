74aa46377e6bffc2e3fc231907303d1d6bf0fd77
GS-719 <134775577+GS-719@users.noreply.github.com>
2025-06-14 16:25:19 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,34 @@
+'use client';
 import React from 'react';
 import "../styles/Navbar.css";
 import Logo from "@/Img/Logo/LOGO.png";
 import Image from 'next/image';
 import Link from "next/link";
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
+    const [darkMode, setDarkMode] = useState(false);
+
+    useEffect(() => {
+        const storedMode = localStorage.getItem('theme');
+        if (storedMode === 'dark') {
+            setDarkMode(true);
+        }
+    }, []);
+    useEffect(() => {
+        const root = document.documentElement;
+        if (darkMode) {
+            root.classList.add('dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            root.classList.remove('dark');
+            localStorage.setItem('theme', 'light');
+        }
+    }, [darkMode]);
+
     return (
         <>
+            <hr />
             <header>
                 <Link href="/" className='link'>
                     <div className="logo">
@@ -26,6 +48,9 @@ const Navbar = () => {
                             <Link className='link' href="mailto:gstechinfo719@gmail.com">Contact US</Link>
                         </li>
                     </ul>
+                    <button className='drTBtn' aria-label="Toggle dark mode" onClick={() => setDarkMode(!darkMode)}>
+                        {darkMode ? '🌞' : '🌙'}
+                    </button>
                 </nav>
             </header>
             <hr />
